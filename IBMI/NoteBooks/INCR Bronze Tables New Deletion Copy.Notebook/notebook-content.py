1025bd51f01c214e25ab7b82abbe2a72356868e7
ravipenmetsastevens <Raju.Penmesta@stevenstransport.com>
2026-04-17 16:47:26 -0500

    Committing 4 items from workspace a6d2c31b-0b03-4c60-a258-c2664c56fe3d

---
+++ IBMI/NoteBooks/INCR Bronze Tables New Deletion Copy.Notebook/notebook-content.py
@@ -0,0 +1,89 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "f9a73da1-08e4-4f3d-a67f-74623bfde721",
+# META       "default_lakehouse_name": "data_central_lh",
+# META       "default_lakehouse_workspace_id": "a6d2c31b-0b03-4c60-a258-c2664c56fe3d",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f9a73da1-08e4-4f3d-a67f-74623bfde721"
+# META         },
+# META         {
+# META           "id": "116d22a7-a4dd-4285-8f7e-411dd6d99a46"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("DELETE FROM ibmi_incr_order_bronze_new")
+spark.sql("DELETE FROM ibmi_incr_cd_order_bronze_new")
+spark.sql("DELETE FROM ibmi_incr_tlb_order_bronze_new")
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("DELETE FROM ibmi_incr_load_bronze_new")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("DELETE FROM ibmi_incr_arfile_bronze")
+spark.sql("DELETE FROM ibmi_incr_cd_arfile_bronze")
+spark.sql("DELETE FROM ibmi_incr_tlb_arfile_bronze")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("DELETE FROM ibmi_incr_billing_bronze_new")
+spark.sql("DELETE FROM ibmi_incr_cd_billing_bronze_new")
+spark.sql("DELETE FROM ibmi_incr_tlb_billing_bronze_new")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
---
+++ INCR Bronze Tables New Deletion 2.Notebook/notebook-content.py
@@ -0,0 +1,54 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "f9a73da1-08e4-4f3d-a67f-74623bfde721",
+# META       "default_lakehouse_name": "data_central_lh",
+# META       "default_lakehouse_workspace_id": "a6d2c31b-0b03-4c60-a258-c2664c56fe3d",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f9a73da1-08e4-4f3d-a67f-74623bfde721"
+# META         },
+# META         {
+# META           "id": "116d22a7-a4dd-4285-8f7e-411dd6d99a46"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("DELETE FROM ibmi_incr_utlfile_bronze")
+spark.sql("DELETE FROM ibmi_incr_earnings_history_bronze")
+spark.sql("DELETE FROM ibmi_incr_drivers")
+spark.sql("DELETE FROM ibmi_incr_drivers_bio")
+
+spark.sql("DELETE FROM ibmi_incr_stopoff_bronze")
+spark.sql("DELETE FROM ibmi_incr_driver_home_history_bronze")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
